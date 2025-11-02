403dac786ceffe2873e7c0e0ce7200f8c5c2183a
Chandu-Collab <chandrahasareddy65@gmail.com>
2025-11-02 17:27:27 +0530

    working

---
+++ client/public/service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'haunted-ai-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/ghost-icon-192.png',
+  '/ghost-icon-512.png',
+  // Add more assets as needed
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
