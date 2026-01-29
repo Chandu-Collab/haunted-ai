6644276e9aa8d1020e158c85a9c2141d07a50c04
Chandu-Collab <chandrahasareddy65@gmail.com>
2026-01-29 09:24:07 +0530

    Final checks

--- server/testRedisConnection.js
+++ server/testRedisConnection.js
@@ -1,4 +1,10 @@
 const { createClient } = require('redis');
+const dotenv = require('dotenv');
+const path = require('path');
+
+// Explicitly load .env file
+const envPath = path.resolve(__dirname, './.env');
+dotenv.config({ path: envPath });
 
 const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
 const redisClient = createClient({ url: redisUrl });
